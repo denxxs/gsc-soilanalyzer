c683d304e7fdbdca2782cb66052bad495ce07a82
quantum2511 <bhavya.chanana2021@vitstudent.ac.in>
2024-02-14 01:26:12 +0530

    weather API

---
+++ config.py
@@ -0,0 +1,1 @@
+weather_api_key = "0b8c24d4cbabaed96fb4da4a7d497c8c"
---
+++ weather.py
@@ -0,0 +1,33 @@
+import requests
+import config
+
+def weather_fetch(city_name):
+    """
+    Fetch and returns the temperature and humidity of a city
+    :params: city_name
+    :return: temperature, humidity
+    """
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+
+    x = response.json()
+    print(x)
+
+    if x["cod"] != "404":
+        y = x["main"]
+        
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        # rain = x["rain"] # rain if exists it will show up in x -> api json response
+        return temperature, humidity, rain
+    else:
+        return None
+    
+
+temp, hum, rain = weather_fetch("Boston")
+print("temp:", temp)
+print("humidity:", hum)
+print("rain:", rain)
